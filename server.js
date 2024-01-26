b8db507973e12b12fd1fa2002b4b0636b89dfee7
vmasheva <vmasheva@gmail.com>
2024-01-26 11:44:35 +0000

    sync hash

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(res);
+
 //END_SYNC
 
 app.listen(process.env.PORT || 3000, () => {});
